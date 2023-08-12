0b8ae5197ebb7cbf728e5d5233f482935fd5d950
Luke <694014820@qq.co>
2023-08-12 17:22:15 +0800

    m

--- app/src/main/java/com/example/myapplication/mediaoverlay/ViewTreeRenderDelegate.kt
+++ app/src/main/java/com/example/myapplication/mediaoverlay/ViewTreeRenderDelegate.kt
@@ -10,7 +10,6 @@ import android.view.Surface
 import android.view.View
 import android.view.WindowManager
 import android.widget.Space
-import kotlinx.coroutines.yield
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
