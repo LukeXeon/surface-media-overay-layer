66bfea36ed9b3d80d464521c8743f27e4380bf20
Luke <694014820@qq.co>
2023-08-19 18:55:13 +0800

    m

--- app/src/main/java/android/magic/research/surfacezorder/CertaintySurfaceZOrderLayout.kt
+++ app/src/main/java/android/magic/research/surfacezorder/CertaintySurfaceZOrderLayout.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.util.AttributeSet
 import android.view.SurfaceView
 import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import androidx.core.view.allViews
 import androidx.core.view.ancestors
@@ -14,7 +15,7 @@ class CertaintySurfaceZOrderLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
     companion object {
-        private val sReAttachToWindowMethodSequence by lazy {
+        private val RE_ATTACH_TO_WINDOW_METHOD_SEQUENCE by lazy {
             sequenceOf(
                 "onDetachedFromWindow",
                 "onAttachedToWindow"
@@ -28,60 +29,82 @@ class CertaintySurfaceZOrderLayout @JvmOverloads constructor(
         }
     }
 
-    private var mSkipSort = false
-    private val mSortedSurfaceViews = WeakHashMap<SurfaceView, Int>()
-    private val mTempSurfaceViews = Array(2) { ArrayList<SurfaceView>() }
+    private var mHasNewLayout = false
+    private lateinit var mSortedSurfaceViews: WeakHashMap<SurfaceView, Int>
+    private lateinit var mTempSurfaceViews: Array<ArrayList<SurfaceView>>
+    private lateinit var mOnGlobalLayoutListener: ViewTreeObserver.OnGlobalLayoutListener
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        if (!mSkipSort) {
-            mTempSurfaceViews[0].clear()
-            mTempSurfaceViews[0].addAll(allViews.filterIsInstance<SurfaceView>())
+        mHasNewLayout = true
+    }
+
+    private fun onPostLayout() {
+        if (!this::mTempSurfaceViews.isInitialized) {
+            mTempSurfaceViews = Array(2) { ArrayList(childCount) }
+        }
+        mTempSurfaceViews[0].addAll(allViews.filterIsInstance<SurfaceView>())
+        if (!this::mSortedSurfaceViews.isInitialized) {
+            mSortedSurfaceViews = WeakHashMap()
+        } else {
             mTempSurfaceViews[1].ensureCapacity(mSortedSurfaceViews.size)
             mTempSurfaceViews[1].addAll(
                 mSortedSurfaceViews.entries.asSequence().sortedBy { it.value }.map { it.key }
             )
-            if (mTempSurfaceViews[0] != mTempSurfaceViews[1]) {
-                for (index in mTempSurfaceViews[0].indices) {
-                    mSortedSurfaceViews[mTempSurfaceViews[0][index]] = index
-                    val order = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-                        // 从8.0开始系统悄悄的改变了排序规则
-                        mTempSurfaceViews[0].lastIndex - index
-                    } else {
-                        index
-                    }
-                    val surfaceView = mTempSurfaceViews[0][order]
-                    val tempVisibility = surfaceView.visibility
-                    surfaceView.visibility = View.GONE
-                    sReAttachToWindowMethodSequence.runCatching {
-                        forEach {
-                            it.invoke(surfaceView)
-                        }
-                    }
-                    surfaceView.visibility = tempVisibility
-                }
-                viewTreeObserver.dispatchOnPreDraw()
-            }
-            mTempSurfaceViews.forEach {
-                it.clear()
-            }
         }
-    }
-
-    private fun clearAllState() {
+        if (mTempSurfaceViews.all { it.isNotEmpty() } && mTempSurfaceViews[0] != mTempSurfaceViews[1]) {
+            for (index in mTempSurfaceViews[0].indices) {
+                mSortedSurfaceViews[mTempSurfaceViews[0][index]] = index
+                val order = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                    // 从8.0开始系统悄悄的改变了排序规则
+                    mTempSurfaceViews[0].lastIndex - index
+                } else {
+                    index
+                }
+                val surfaceView = mTempSurfaceViews[0][order]
+                val tempVisibility = surfaceView.visibility
+                surfaceView.visibility = View.GONE
+                RE_ATTACH_TO_WINDOW_METHOD_SEQUENCE.runCatching {
+                    forEach {
+                        it.invoke(surfaceView)
+                    }
+                }
+                surfaceView.visibility = tempVisibility
+            }
+            viewTreeObserver.dispatchOnPreDraw()
+        }
         mTempSurfaceViews.forEach {
             it.clear()
         }
-        mSortedSurfaceViews.clear()
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        mSkipSort = ancestors.any { it is CertaintySurfaceZOrderLayout }
+        if (!ancestors.any { it is CertaintySurfaceZOrderLayout }) {
+            if (!this::mOnGlobalLayoutListener.isInitialized) {
+                mOnGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+                    if (mHasNewLayout) {
+                        mHasNewLayout = false
+                        onPostLayout()
+                    }
+                }
+            }
+            viewTreeObserver.addOnGlobalLayoutListener(mOnGlobalLayoutListener)
+        }
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        clearAllState()
+        if (this::mOnGlobalLayoutListener.isInitialized) {
+            viewTreeObserver.removeOnGlobalLayoutListener(mOnGlobalLayoutListener)
+        }
+        if (this::mTempSurfaceViews.isInitialized) {
+            mTempSurfaceViews.forEach {
+                it.clear()
+            }
+        }
+        if (this::mSortedSurfaceViews.isInitialized) {
+            mSortedSurfaceViews.clear()
+        }
     }
 }
